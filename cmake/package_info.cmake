6f865d11c7ebc505038ac8e28ab724fbc6866cd7
Angelo Theodorou <encelo@gmail.com>
2021-02-07 02:03:49 +0100

    Integrate latest changes from the template project
    - Copy MSVC libraries only in the target directory
    - Set Visual Studio debugger working directory to the target directory
    - Add a custom CMake target to ensure the target directory exists
    - Fix package configuration remapping for multi-configuration generators
    - Update copyright dates to 2021

--- cmake/package_info.cmake
+++ cmake/package_info.cmake
@@ -1,7 +1,7 @@
 set(PACKAGE_NAME "ncTracer")
 set(PACKAGE_EXE_NAME "nctracer")
 set(PACKAGE_VENDOR "Angelo Theodorou")
-set(PACKAGE_COPYRIGHT "Copyright ©2019-2020 ${PACKAGE_VENDOR}")
+set(PACKAGE_COPYRIGHT "Copyright ©2019-2021 ${PACKAGE_VENDOR}")
 set(PACKAGE_DESCRIPTION "pmTracer front-end")
 set(PACKAGE_HOMEPAGE "https://ncine.github.io")
 set(PACKAGE_REVERSE_DNS "io.github.ncine.nctracer")
